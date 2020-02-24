6431bdcd63f2e0183d7d92f8218ef375550580e0
UchiTesting <uchitesting57@gmail.com>
2020-02-24 16:50:21 +0100

    Rfactored code on exercise 16.

---
+++ 200221-Exo16 Deck Manager/Trumps/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _200221_Exo16_Deck_Manager.Trumps
+{
+    class Deck
+    {
+        List<DeckTrump> deckTrumps = null;
+        private static Deck Instance = null;
+
+        protected Deck(){}
+
+        public Deck GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new Deck();
+            }
+
+            return Instance;
+        }
+
+        public void Add(Trump t)
+        {
+        }
+        
+    }
+}
---
+++ 200221-Exo16 Deck Manager/Trumps/DeckTrump.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _200221_Exo16_Deck_Manager.Trumps
+{
+    abstract class DeckTrump
+    {
+        Trump t = null;
+        short instances;
+        int maxInstances;
+
+        DeckTrump(Trump t, int maxInstances = 4)
+        {
+            this.t = t;
+            instances = 0;
+        }
+
+        public void AddInstance()
+        {
+            if (instances < maxInstances)
+            {
+                instances++;
+            }
+        }
+
+        public void RemoveInstance()
+        {
+            if (instances > 0)
+            {
+                instances--;
+            }
+        }
+
+        public void UpdateTrump(Trump t)
+        {
+            this.t = t;
+        }
+    }
+}
